17c1612bbaa5112d04eac006f161995cfd233e0c
Rana Huzaima Imtiaz <huzaimadigital@gmail.com>
2024-07-31 16:01:40 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImageUpload from './components/ImageUpload';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ImageUpload />
+    </div>
+  );
+};
+
+export default App;
